ede365b151431a26d1456c30d902171e76ee2b78
lxorg <lxorg@b1670157-89ef-0310-b5f9-fe94102b68b0>
2005-03-02 15:43:50 +0000

    testen wieder geloesht

--- testen/engine/light.cpp
+++
@@ -1,9 +0,0 @@
-#include ".\light.h"
-
-light::light(void)
-{
-}
-
-light::~light(void)
-{
-}
--- testen/functions.cpp
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-void writeText(int posx, int posy, string givenText, int r, int g, int b)
-{
-	SetBkMode(hDc, TRANSPARENT);
-	SetTextColor(hDc, RGB(r, g, b));
-	TextOut(hDc, posx, posy*12, givenText.c_str(), givenText.length());
-	ReleaseDC(hWnd, hDc);
-}
