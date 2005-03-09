6083adb381a9d7afa298b57ef0866b10a8bd2db9
lxorg <lxorg@b1670157-89ef-0310-b5f9-fe94102b68b0>
2005-03-09 15:51:28 +0000

    Objektstruktur veraendert

--- 3dGame/engine/MyMesh.h
+++ 3dGame/engine/MyMesh.h
@@ -6,7 +6,7 @@
 #include "MyVertex.h"
 #include "MyMasterPosition.h"
 
-class MyMesh
+class MyMesh : public MyMasterPosition
 {
 public:
 	MyMesh(void);
@@ -28,12 +28,6 @@ public:
 								float rotDirZ);
 	HRESULT				load();
 	void				draw();
-	void				move();
-	MyMasterPosition*	getPosition();
-	MyMasterPosition*	getReference();
-	MyVertex*			getScale();
-	void				activateScaling();
-	void				deactivateScaling();
 	void				setReference(MyMasterPosition* givenReference);
 	
 private:
@@ -43,11 +37,7 @@ private:
 	LPD3DXMESH			m_pMesh;
 	D3DMATERIAL9*		m_pMaterials;	//Materialbuffer
 	LPDIRECT3DTEXTURE9*	m_pTextures;	//Texturbuffer
-	MyMasterPosition*	m_pPosition;
-	MyMasterPosition*	m_pReferencePoint;
-	LPDIRECT3DDEVICE9	_D3DDevice;
 	D3DXMATRIX*			_matWorld;
-	bool				m_bScale;
-	MyVertex*			m_pScaleFactor;
+	MyMesh*				m_pMasterObject;
 };
 #endif
