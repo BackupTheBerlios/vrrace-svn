abca69eaa35ab76ca2d575f25d31e7992e5b1fa1
lxorg <lxorg@b1670157-89ef-0310-b5f9-fe94102b68b0>
2005-03-10 16:37:25 +0000

    kleine Formatierungen

--- 3dGame/engine/MyMasterPosition.h
+++ 3dGame/engine/MyMasterPosition.h
@@ -10,38 +10,38 @@ public:
 	MyMasterPosition(void);
 	~MyMasterPosition(void);
 
-	void	move();
-	void	draw();
-	void	calcMatrix(D3DXMATRIX* givenMatrix);	//Masterobjekt aktiviert die Berechnung
-	void	activateScaling();						//Scalierung aktivieren
-	void	deactivateScaling();					//Scalierung deaktivieren
-	void	transform();							//End-Transformation druchfuehren
-	void	calcClients();							//alle Clients die eigene Matrix senden
-	void	setMaster(MyMasterPosition* givenMaster);
-	void	addClient(MyMasterPosition* givenClient);
-	MyVertex*	getScale();
+	void						move();
+	void						draw();
+	void						calcMatrix(D3DXMATRIX* givenMatrix);		//Masterobjekt aktiviert die Berechnung
+	void						activateScaling();							//Scalierung aktivieren
+	void						deactivateScaling();						//Scalierung deaktivieren
+	void						transform();								//End-Transformation druchfuehren
+	void						calcClients();								//alle Clients die eigene Matrix senden
+	void						setMaster(MyMasterPosition* givenMaster);
+	void						addClient(MyMasterPosition* givenClient);
+	MyVertex*					getScale();
 
-	MyVertex*					m_pPosition;		//aktuelle Position
-	MyVertex*					m_pDirection;		//Bewegungsrichtung
-	MyVertex*					m_pRotation;		//aktuelle Rotation/StellWinkel
-	MyVertex*					m_pRotDir;			//Rotationsrichtung
-	MyVertex*					m_pScaleFactor;		//Skalierungsfaktor
+	MyVertex*					m_pPosition;								//aktuelle Position
+	MyVertex*					m_pDirection;								//Bewegungsrichtung
+	MyVertex*					m_pRotation;								//aktuelle Rotation/StellWinkel
+	MyVertex*					m_pRotDir;									//Rotationsrichtung
+	MyVertex*					m_pScaleFactor;								//Skalierungsfaktor
 	LPDIRECT3DDEVICE9			_D3DDevice;
-	int						m_pType;
+	int							m_pType;
 
 private:
-	void						calcOwnMatrix();	//eigene Matrix berechnen
+	void						calcOwnMatrix();							//eigene Matrix berechnen
 
-	D3DXMATRIX*					m_pScaleMatrix;			//Skalierung
-	D3DXMATRIX*					m_pTranslationMatrix;	//Translierung
-	D3DXMATRIX*					m_pRotationMatrix;		//Rotation
-	D3DXMATRIX*					m_pMatrix;				//lokale Transformation
-	D3DXMATRIX*					m_pFinalMatrix;			//finale Transformation
+	D3DXMATRIX*					m_pScaleMatrix;								//Skalierung
+	D3DXMATRIX*					m_pTranslationMatrix;						//Translierung
+	D3DXMATRIX*					m_pRotationMatrix;							//Rotation
+	D3DXMATRIX*					m_pMatrix;									//lokale Transformation
+	D3DXMATRIX*					m_pFinalMatrix;								//finale Transformation
 
 	vector<MyMasterPosition*>	m_pClients;
 	MyMasterPosition*			m_pMaster;
 
-	bool				m_bScale;	
+	bool						m_bScale;	
 
 };
 #endif
